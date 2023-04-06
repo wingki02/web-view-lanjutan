c2def3c34f11ade4b5460479d8ed11405130cf54
Wingki <wingkisandros@gmail.com>
2023-04-06 09:55:10 +0700

    Initial commit

---
+++ app/src/main/java/com/wingki_202102318/webviewlanjutan/WebAppInteface.java
@@ -0,0 +1,32 @@
+package com.wingki_202102318.webviewlanjutan;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+ class WebAppInterface
+{
+    private Activity _activity;
+    private Context _context;
+
+    public WebAppInterface(Context context, Activity activity)
+    {
+        _context = context;
+        _activity = activity;
+    }
+    @JavascriptInterface
+    public void showToast(String message)
+    {
+        Toast.makeText(_context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @JavascriptInterface
+    public void showSms()
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING);
+        _context.startActivity(intent);
+    }
+}
